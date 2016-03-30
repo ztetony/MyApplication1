16607f4c0caa3ee6cb36777a0d24b3dc9627e25a
hao200 <njluhl@sohu.com>
2016-03-30 20:15:16 +0800

    fromas c++ by tony

---
+++ app/src/androidTest/java/com/example/blt/myapplication1/NewCppClass.h
@@ -0,0 +1,14 @@
+//
+// Created by blt on 2016/3/30.
+//
+
+#ifndef MY_APPLICATION1_NEWCPPCLASS_H
+#define MY_APPLICATION1_NEWCPPCLASS_H
+
+
+class NewCppClass {
+
+};
+
+
+#endif //MY_APPLICATION1_NEWCPPCLASS_H
